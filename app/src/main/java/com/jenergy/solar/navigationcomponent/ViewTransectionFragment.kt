698392f031277f945182b9ebe979fd76a785e2bd
Waseem Shahzad <w.shahzad605@gmail.com>
2022-01-29 15:56:59 +0500

    2nd lec

---
+++ app/src/main/java/com/jenergy/solar/navigationcomponent/ViewTransectionFragment.kt
@@ -0,0 +1,14 @@
+package com.jenergy.solar.navigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ViewTransectionFragment : Fragment(R.layout.fragment_trasection_view) {
+
+
+
+}
